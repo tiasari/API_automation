77abf71078bad87a3fa9d34850ebc4953e586ca3
tiasari <thiyashyari@gmail.com>
2022-04-23 20:03:27 +0700

    Test Get 1 By Tia

---
+++ Test Suites/getData.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>getData</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d67805b5-45c5-4ceb-ad36-20eca0ed8ee2</testSuiteGuid>
+</TestSuiteEntity>
